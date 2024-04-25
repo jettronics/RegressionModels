1949b6293c9ac2d211b52b440e1898824a3ed531
Jens Thielemann <jens.thielemann@gmail.com>
2024-04-25 20:04:36 +0200

    Housing example edited

--- Examples/Linear Regression - Housing/regression_linear_housing.m
+++ Examples/Linear Regression - Housing/regression_linear_housing.m
@@ -7,11 +7,11 @@ V = 12;
 x = zeros(12,L);
 x(1,:) = R(:,2)' * 0.092903; %area: square feet to qm
 x(2,:) = R(:,3)'; %bedrooms
-x(3,:) = R(:,4)'; %bathrooms 
+x(3,:) = R(:,4)'; %bathrooms
 x(4,:) = R(:,5)'; %stories: number of levels
 x(5,:) = R(:,6)'; %main road: house faces main road
-x(6,:) = R(:,7)'; %guestroom: no: 0, yes: 1 
-x(7,:) = R(:,8)'; %basement: no: 0, yes: 1 
+x(6,:) = R(:,7)'; %guestroom: no: 0, yes: 1
+x(7,:) = R(:,8)'; %basement: no: 0, yes: 1
 x(8,:) = R(:,9)'; %hotwaterheating: house uses gas: no: 0, yes: 1
 x(9,:) = R(:,10)'; %airconditioning: no: 0, yes: 1
 x(10,:) = R(:,11)'; %parking: number of parking lots
@@ -21,6 +21,10 @@ y = R(:,1)' * 0.011; %price: indian rupie to euro
 %x1 = (0.5:0.25:5.25);
 %x2 = (0.2:0.1:2.1);
 %y = zeros(1,size(x1));
+
+r = R;
+r(:,1) = R(:,1) * 0.011;
+r(:,2) = R(:,2) * 0.092903;
 
 mean = zeros(1,V);
 mean_s = zeros(1,V);
@@ -49,19 +53,23 @@ for i=1:L
    X(i,7) = x_s(6,i);
    X(i,8) = x_s(7,i);
    X(i,9) = x_s(8,i);
-   X(i,10) = x_s(9,i);   
+   X(i,10) = x_s(9,i);
    X(i,11) = x_s(10,i);
    X(i,12) = x_s(11,i);
-   X(i,13) = x_s(12,i);     
+   X(i,13) = x_s(12,i);
 end
 
-beta = 0.00002;
-lambda = [0.0001; 0.00001; 0.00001; 0.00001; 0.00001; 0.00001; 0.00001; 0.00001; 0.00001; 0.00001; 0.00001; 0.00001; 0.00001;];
+beta = 0.0002;
+%lambda = [0.0; 0.0; 0.0; 0.0; 0.0; 0.0; 0.0; 0.0; 0.0; 0.0; -0.002; 0.0; 0.0;];
+lambda = [0.0; 0.0; 0.0; 0.0; 0.0; 0.0; 0.0; 0.0; 0.0; 0.0; 0.0; 0.0; 0.0;];
 %lambda = [0.0; 0.0; 0.0;];
 
 % Bayes estimation
-for i=1:1500
+it = 200;
+h_trend = zeros(V+1,it);
+for i=1:it
   h = h - beta * ((X' * X * h) - (X' * y') + (lambda * h' * h ));
+  h_trend(:,i) = h;
 end
 
 % Regression result
@@ -74,8 +82,8 @@ p(2) = 2; %bedrooms
 p(3) = 2; %bathrooms
 p(4) = 2; %stories: number of levels
 p(5) = 0; %main road: house faces main road
-p(6) = 2; %guestroom: no: 0, yes: 1 
-p(7) = 1; %basement: no: 0, yes: 1 
+p(6) = 2; %guestroom: no: 0, yes: 1
+p(7) = 1; %basement: no: 0, yes: 1
 p(8) = 1; %hotwaterheating: house uses gas: no: 0, yes: 1
 p(9) = 0; %airconditioning: no: 0, yes: 1
 p(10) = 4; %parking: number of parking lots
@@ -100,4 +108,30 @@ for i = 1 : V+1
   %H(i) = bar( z(i), h(i), 0.4, 'facecolor', colours{i} );
   H(i) = bar( z(i), h(i), 0.4 );
 endfor
+grid on
+
+figure(2)
+clf
+plot(h_trend(2,:))
+hold on
+plot(h_trend(3,:))
+hold on
+plot(h_trend(4,:))
+hold on
+plot(h_trend(5,:))
+hold on
+grid on
+ylim([0 8000])
+xlabel('Iterations')
+legend('Area in qm', 'Number of bedrooms','Number of bathrooms','Number of stories')
+
+figure(3)
+clf
+subplot(2,1,1)
+plot(x(10,:),y,'ok')
+hold on
+grid on
+subplot(2,1,2)
+plot(X(:,11),Y_r,'ok')
+hold on
 grid on
